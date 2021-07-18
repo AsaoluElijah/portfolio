a91bf02b7cfad339be2c6cd1d57d3e4d213df786
Asaolu Elijah <asaoluelijah7@gmail.com>
2021-07-18 18:57:29 +0100

    created link component :fire:

---
+++ components/SocialLink.vue
@@ -0,0 +1,16 @@
+<template>
+  <a v-if="to !== ''" :href="to" target="_blank" rel="noopener">
+    <slot />
+  </a>
+</template>
+<script>
+export default {
+  props: {
+    to: {
+      type: String,
+      required: true,
+      default: ""
+    }
+  }
+};
+</script>
