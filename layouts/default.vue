a92887649e35db5fa61ed0dc1d2571310f82264a
Asaolu Elijah <asaoluelijah7@gmail.com>
2021-07-18 18:31:41 +0100

    removed import component statement, this is Nuxt :)

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,16 +6,3 @@
     </div>
   </div>
 </template>
-<script>
-import NavBar from "../components/NavBar";
-export default {
-  components: {
-    NavBar: NavBar
-  }
-};
-</script>
-<style scoped>
-#app {
-  margin-top: 100px;
-}
-</style>
